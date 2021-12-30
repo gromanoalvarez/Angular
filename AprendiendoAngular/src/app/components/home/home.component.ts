9ed1c8a2961f84b179d899581b06089295850d83
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2021-12-30 15:41:54 -0300

    Retoques nav para navegar de manera instantanea entre los componentes de nuestra Aplicacion Web

--- AprendiendoAngular/src/app/components/home/home.component.ts
+++ AprendiendoAngular/src/app/components/home/home.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+
+  constructor() {
+    this.title = "Últimos artículos";
+   }
 
   ngOnInit(): void {
   }
