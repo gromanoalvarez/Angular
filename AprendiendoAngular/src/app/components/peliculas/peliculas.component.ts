a9049d87172cc17949bd4d628d8633670ddc2b1f
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-01-04 01:37:52 -0300

    Modelos o clases en angular

--- AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,5 +1,5 @@
 import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
-
+import { Pelicula } from '../../models/pelicula';
 @Component({
   selector: 'peliculas', //nombre de la etiqueta en la vista
   templateUrl: './peliculas.component.html',
@@ -9,17 +9,23 @@ import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
   public titulo: string;
-  public peliculas: Array<any>; //array de objetos con propiedad titulo e imagen
+  public peliculas: Pelicula[]; //array de objetos con propiedad titulo e imagen
 
   constructor() {
     this.titulo = "PELÍCULAS: El constructor carga este valor en la propiedad titulo";
-    this.peliculas = [
-      { year: 2021, title: "Spiderman 4: Verse", image: 'https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/08/Nueva-e-irresistible-pista-del-Spider-Verse-de-No-Way-Home-aparece-gracias-al-guionista-de-Spider-Man-into-the-Spiderverse.jpg?fit=1280%2C720&quality=80&ssl=1'},
-      { year: 2018, title: "Los vengadores Endgame", image: 'https://r1.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic4.abc.es%2Fmedia%2Fpeliculas%2F000%2F052%2F759%2Fvengadores-endgame-1.jpg&nuevoancho=690&medio=abc'},
-      { year: 2011, title: "Batman vs Superman", image: 'https://i0.wp.com/elrincon.tv/wp-content/uploads/2016/03/batman_v_superman.jpg?resize=660%2C330&ssl=1'},
-      { year: 2013, title: "Batman vs Superman 2", image: 'https://i0.wp.com/elrincon.tv/wp-content/uploads/2016/03/batman_v_superman.jpg?resize=660%2C330&ssl=1'}
+    // this.peliculas = [ // SE PODRIA HACER ASI "CON ARRAY DE OBJETOS" PERO SE HACE MUY LARGO Y POCO PRACTICO POR ESO CREO "MODELO" (o clase como molde para cada pelicula)
+    //   { year: 2021, title: "Spiderman 4: Verse", image: 'https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/08/Nueva-e-irresistible-pista-del-Spider-Verse-de-No-Way-Home-aparece-gracias-al-guionista-de-Spider-Man-into-the-Spiderverse.jpg?fit=1280%2C720&quality=80&ssl=1'},
+    //   { year: 2018, title: "Los vengadores Endgame", image: 'https://r1.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic4.abc.es%2Fmedia%2Fpeliculas%2F000%2F052%2F759%2Fvengadores-endgame-1.jpg&nuevoancho=690&medio=abc'},
+    //   { year: 2011, title: "Batman vs Superman", image: 'https://i0.wp.com/elrincon.tv/wp-content/uploads/2016/03/batman_v_superman.jpg?resize=660%2C330&ssl=1'},
+    //   { year: 2013, title: "Batman vs Superman 2", image: 'https://i0.wp.com/elrincon.tv/wp-content/uploads/2016/03/batman_v_superman.jpg?resize=660%2C330&ssl=1'}
+    // ];
+        this.peliculas = [ // USANDO MODELO O CLASE PELICULA
+      new Pelicula("Spiderman 4: Verse", 2021, 'https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/08/Nueva-e-irresistible-pista-del-Spider-Verse-de-No-Way-Home-aparece-gracias-al-guionista-de-Spider-Man-into-the-Spiderverse.jpg?fit=1280%2C720&quality=80&ssl=1'),
+      new Pelicula("Los vengadores: Endgame", 2018, 'https://r1.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic4.abc.es%2Fmedia%2Fpeliculas%2F000%2F052%2F759%2Fvengadores-endgame-1.jpg&nuevoancho=690&medio=abc'),
+      new Pelicula("Batman vs Superman", 2011, 'https://i0.wp.com/elrincon.tv/wp-content/uploads/2016/03/batman_v_superman.jpg?resize=660%2C330&ssl=1')
     ];
   }
+ 
 
   ngOnInit(): void {
     console.log("Evento onInit, componente iniciado. Aca puede ir lógica y funcionalidad")
---
+++ AprendiendoAngular/src/app/models/pelicula.ts
@@ -0,0 +1,37 @@
+ /**
+   * PRACTICA POO
+   * Un MODELO es una CLASE que representa a un tipo de OBJETO PELICULA, objetos con mismas propiedades pero de valores diferentes
+   * (sera utilizado en peliculas.components.ts)
+   */
+
+export class Pelicula{ //EXPORT da la oportunidad de utilizar la clase fuera de este fichero
+
+    /**Las propiedades pueden tener 3 valores de acceso:
+    PUBLIC: se pueden utilizar tanto dentro como fuera de la clase 
+    PROTECTED: dentro de la clase y las clases que la hereden
+    PRIVATE: solo se pueden utilizar dentro de la clase donde fueron definidas
+    */
+
+    //1ra opcion... Lo podria hacer "clasicamente" de este modo,
+/*
+    public title: string;
+    public year: number;
+    public image: string;
+
+    constructor(title, year, image){
+        this.title=title;
+        this.year=year;
+        this.image=image;
+    }
+
+*/
+ //2da opcion-... Que nos permite TYPESCRIPT
+
+ constructor(public title: string, public year: number, public image: string){
+     this.title=title;
+     this.year=year;
+     this.image=image;
+ }
+
+
+ }
