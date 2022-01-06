7e1285d9980b50c61d05aeae7aec2733e2e76812
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-01-06 03:04:46 -0300

    Mostrar listado de articles en el Blog

--- AprendiendoAngular/src/app/components/blog/blog.component.ts
+++ AprendiendoAngular/src/app/components/blog/blog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/models/article';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-blog',
@@ -9,16 +11,25 @@ import { ArticleService } from 'src/app/services/article.service';
 })
 export class BlogComponent implements OnInit {
 
+  public articles!: Article[];
+  public url: string;
+
   constructor(
     private _articleService: ArticleService
-  ) { }
+  ) { 
+    this.url= Global.url;
+  }
 
   ngOnInit(): void {
     //subscribe es un metodo del Observable getArticles que permite recoger los datos
     //que devuelve las peticiones http
     this._articleService.getArticles().subscribe(
       {
-        next: (response) => console.log(response),
+        next: (response) => {
+          if(response.articles){
+          this.articles = response.articles;
+          }
+        },
         error: (error) => console.log(error)
       }
     );
