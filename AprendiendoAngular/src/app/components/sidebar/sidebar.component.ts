2669aa5eea05b497b5bcbf0c89e0735332fa6c46
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-01-08 00:04:37 -0300

    Enlazar el search con el form del sidebar

--- AprendiendoAngular/src/app/components/sidebar/sidebar.component.ts
+++ AprendiendoAngular/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public searchString!: string; //lo voy a rellenar por el ngModel de la vista
+
+  constructor(private _router:Router, private _route:ActivatedRoute) { 
+  }
 
   ngOnInit(): void {
+  }
+
+  goSearch(){
+    // para que me redirija tengo que usar el import de ruter 
+    this._router.navigate(['/buscar', this.searchString]);
   }
 
 }
