2f5a5e39653609c4e9d8dbf3ca5aecfcbd368565
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-02-24 17:47:40 -0300

    Mejoras en Responsive

--- AprendiendoAngular/src/app/components/slider/slider.component.ts
+++ AprendiendoAngular/src/app/components/slider/slider.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent implements OnInit {
-  //decorador @Input
   @Input() directivaNombre: string;
   @Input() size: string;
 
